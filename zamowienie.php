bd8e8db659c24cd437dd1c6eef392977d098c316
P1LKARZ <91729018+P1LKARZ@users.noreply.github.com>
2023-11-30 09:35:23 +0100

    Add files via upload

---
+++ zamowienie.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styl.css">
+    <title>Sklep</title>
+</head>
+<body>
+    <div id="baner">
+        <h1>Ozdoby-sklep</h1>
+    </div>
+
+    <div id="lewy">
+        <h2>OZDOBY</h2>
+        <a href="galeria.html">Galeria</a>
+        <a href="zamowienie.php">Zamówienie</a>
+    </div>
+
+    <div id="srodkowy">
+        <p>Dodaj użytkownika</p>
+        <form action="" method="post">
+            Imię:<input type="text" name="imie" id="imie"><br>
+            Nazwisko:<input type="text" name="nazwisko" id="nazwisko"><br>
+            e-mail:<input type="email" name="mail" id="mail"><br>
+            <input type="submit" value="WYŚLIJ">
+
+        </form>
+    </div>
+
+    <div id="prawy">
+       <img src="animacja.gif" >
+       
+    </div>
+
+    <footer>
+        <h3>Autor strony:000000000000</h3>
+    </footer>
+</body>
+</html>
